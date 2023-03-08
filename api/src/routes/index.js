d1d221a7cbb52dc0898a4beef9f01eae327fb60d
santiago <santielectro22@gmail.com>
2023-03-08 16:13:54 -0500

    s

---
+++ api/src/routes/index.js
@@ -0,0 +1,10 @@
+const {
+  getProductsHandler
+} = require('../handlers/index')
+
+const { Router } = require('express')
+const router = Router()
+
+router.get('/products', getProductsHandler)
+
+module.exports = router
